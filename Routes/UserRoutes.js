5a30498ad7d2a9754e8179c9bbcaac36118e7c7f
Chanuka999 <chanukaranditha99@gmail.com>
2024-11-13 15:55:23 +0530

    update

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -6,5 +6,9 @@ const User = require("../Model/UserModel");
 const UserController = require("../Controllers/UserControllers");
 
 router.get("/",UserController.getAllUsers);
+router.post("/",UserController.addUsers);
+router.get("/:id",UserController.getById);
+router.put("/:id",UserController.updateUser);
+router.delete("/:id",UserController.deleteUser);
 
 module.exports = router;
