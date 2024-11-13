f19c5dc051fe30fe12ee176ed1f6b5e7827e51cb
Chanuka999 <chanukaranditha99@gmail.com>
2024-11-13 19:00:24 +0530

    Nav bar conected

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
 
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home/Home';
+import AddUser from './Components/AddUser/AddUser';
+import UserDetails from './Components/UserDetails/UserDetails';
+
 
 function App() {
   return (
     <div className="App">
-     <h1>hello</h1>
-     hello
+    <React.Fragment>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/mainhome" element={<Home/>}/>
+        <Route path="/AddUser" element={<AddUser/>}/>
+        <Route path="/UserDetails" element={<UserDetails/>}/>
+      </Routes>
+    </React.Fragment>
+
     </div>
   );
 }
