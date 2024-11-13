01df393b73261d0068e7cd87e61003323df3a854
Chanuka999 <chanukaranditha99@gmail.com>
2024-11-13 12:29:43 +0530

    update

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 //pass=eKwASCHzdQ92KgOP
 const express = require("express");
 const mongoose = require("mongoose");
+const router = require("./Routes/UserRoutes");
 
 const app = express();
 
-app.use("/",(req,res,next) =>{
-    res.send("It is working....");
-})
+app.use("/users",router);
 
 mongoose.connect("mongodb+srv://admin:eKwASCHzdQ92KgOP@cluster0.md3oc.mongodb.net/")
 .then(() => console.log("connected to mongodb"))
