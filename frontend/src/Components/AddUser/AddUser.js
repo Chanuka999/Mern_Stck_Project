af202df2c908ff80a44f8b901244a5968894e1d8
Chanuka999 <chanukaranditha99@gmail.com>
2024-11-14 23:26:02 +0530

    add form

--- frontend/src/Components/AddUser/AddUser.js
+++ frontend/src/Components/AddUser/AddUser.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react'
 import Nav from '../Nav/Nav';
+import { useNavigate } from 'react-router-dom';
+
 
 function AddUser() {
+    const history = useNavigate();
+    const [inputs,setInputs] = useState({
+      name:"",
+      gmail:"",
+      age:"",
+      address:"",
+    })
   return (
     <div>
         <Nav/>
-      <h1>Add User</h1>
+      <h1>Add user page</h1>
+        <form>
+            <label>name:</label>
+            <br/>
+            <input type="text" name="name" required/>
+            <br></br>
+            <label for="lname">Last name:</label>
+            <br/>
+            <input type="text" id="lname" name="lname"/>
+       </form>
     </div>
   )
 }
