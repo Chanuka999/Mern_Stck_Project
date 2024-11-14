63a967622174ffef136c58c1de4bab1ef8aa89b1
Chanuka999 <chanukaranditha99@gmail.com>
2024-11-14 11:42:18 +0530

    update

--- frontend/src/Components/UserDetails/UserDetails.js
+++ frontend/src/Components/UserDetails/UserDetails.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Nav from '../Nav/Nav';
+import axios from "axios";
+
+const URL = "http://localhost:5000/users";
+
+const fetchHandler = async() =>{
+  return await axios.get(URL).then((res) => res.data);
+}
 
 function UserDetails() {
   return (
     <div>
         <Nav/>
-      <h1>User Details</h1>
+      <h1>User Details display page</h1>
     </div>
   )
 }
