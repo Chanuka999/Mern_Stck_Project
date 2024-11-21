11cedad4232c1f1975ec4259815fe6a9164fa92a
Chanuka999 <chanukaranditha99@gmail.com>
2024-11-21 19:06:59 +0530

    update

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import Nav from '../Nav/Nav'
+import React from 'react';
+import Nav from '../Nav/Nav';
+import './Home.css';
 
 function Home() {
   return (
-    <div>
-        <Nav/>
-      <h1>Home new</h1>
+    <div className="home">
+      <Nav />
+      <h1>WELCOME MY HOME PAGE</h1>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
